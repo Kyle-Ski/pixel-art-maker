d62b267522b34ed3198f7af6b63825046d0e1abf
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-09-07 10:44:04 -0600

    created a document devent listner and made the container div

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // create box to hold everything
+    var container = document.createElement ('div')
+    var body = document.querySelector('body')
+    body.appendChild(container).classList.add('container')
+    // create canvas
+    // create color pallet
+})
