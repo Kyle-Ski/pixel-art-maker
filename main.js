cbf96573bdb7dc914a4be2dd74e5afb94fc734cd
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-09-07 11:25:22 -0600

    created inputs and buttons for the rows and columns

--- main.js
+++ main.js
@@ -2,13 +2,31 @@ document.addEventListener('DOMContentLoaded', function(){
     // create box to hold everything
     var container = document.createElement ('div')
     var body = document.querySelector('body')
+    var colorPallet = document.createElement('input')
+    var canvas = document.createElement('div')  
+    var rowsInput = document.createElement('input')
+    var colInput = document.createElement('input')
+    var rowSubmit = document.createElement('button')
+    var colSubmit = document.createElement('button')
     body.appendChild(container).classList.add('container')
     // create canvas
-    var canvas = document.createElement('div')  
     container.append(canvas)
     canvas.classList.add('canvas')
+    canvas.append(rowsInput, colInput)
+    canvas.append(rowSubmit, colSubmit)
+    rowSubmit.innerText = 'Submit'
+    colSubmit.innerText = 'Submit'
+    rowsInput.type = 'text'
+    colInput.type = 'text'
+    rowsInput.placeholder = 'How many rows?'
+    colInput.placeholder = 'How many columns?'
+    
     // create color pallet
-    var colorPallet = document.createElement('div')
     container.append(colorPallet)
-    colorPallet.classList.add('color-pallet')
+    colorPallet.type = 'color'
+    
+    for(let i = 0; i < 148; i++){
+        var canvasBox = document.createElement('div')
+        // var 
+    }
 })
